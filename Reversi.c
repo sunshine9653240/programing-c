2a2b087dc0e2013b297f81e9d1391fdbb1d6a4e6
travis <71485608+sunshine9653240@users.noreply.github.com>
2023-11-14 23:31:04 +0800

    Update Reversi.c

--- Reversi.c
+++ Reversi.c
@@ -3,8 +3,7 @@
 #define WHITE 1 //白棋
 #define BLACK 2 //黑棋
 
-//----------------------------------------------------------------未完成與電腦下 目前是自己跟自己下
-
+//----------------------------------------------------------------下棋溢位
 int chess[8][8]={ //棋盤
     {0,0,0,0,0,0,0,0},
     {0,0,0,0,0,0,0,0},
@@ -16,6 +15,7 @@ int chess[8][8]={ //棋盤
     {0,0,0,0,0,0,0,0}};
 
 int Ltemp=0,Rtemp=0,Utemp=0,Dtemp=0,LUtemp=0,LDtemp=0,RUtemp=0,RDtemp=0;
+int total=0,maxX=0,maxY=0; //紀錄可翻幾顆子 電腦下棋用 
 int newX, newY;
 
 //先定義函式
@@ -23,30 +23,39 @@ int yes_or_no(int,int,int);
 int scanboard();
 void reverse(int,int,int);
 void printchess();
-void computurn();
+void AIturn(int);
 void wherecan(int);
 
 int main() {
     int i=0;
     int yesno = 0;
     int nowcolor=0;
+    int ai_or_user=1;//user 1 ai 0
     printf("要落子的顏色(1=白,2=黑):");
     scanf("%d",&nowcolor);//掃描顏色
 
     while(scanboard()){
         printchess();
-        wherecan(nowcolor);
-
-        yesno = yes_or_no(newY, newX,nowcolor);//呼叫函式 檢查當下要落子的位置是否可行
-        if (yesno == 1){
-            printf("可以落子 要落子嗎(1 or 0):");
-            scanf("%d",&yesno);
-            if(yesno == 1){
-                chess[newY][newX]=nowcolor;
-                reverse(newY, newX, nowcolor);
-                nowcolor=nowcolor%2+1;
+        
+        if (ai_or_user==1){//是誰在下棋
+            wherecan(nowcolor);//檢查何處可落子 讀入落子位置
+            yesno = yes_or_no(newY, newX, nowcolor);//呼叫函式 檢查當下要落子的位置是否可行
+            if (yesno == 1){
+                printf("可以落子 要落子嗎(1 or 0):");
+                scanf("%d",&yesno);
+                if(yesno == 1){
+                    reverse(newY, newX, nowcolor);
+                    nowcolor=nowcolor%2+1;
+                }
             }
+            ai_or_user = 0;
+
+        }else if (ai_or_user==0){
+            AIturn(nowcolor);
+            nowcolor=nowcolor%2+1;
+            ai_or_user = 1;
         }
+        
     }
     return 0;
 }
@@ -56,23 +65,28 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
     Ltemp=0,Rtemp=0,Utemp=0,Dtemp=0,LUtemp=0,LDtemp=0,RUtemp=0,RDtemp=0;
     int i; //迴圈用
     i=0;
-    while(1){
+    
+    while(1){//左
         i++;
-        if (x-i<0||chess[y][x-i]==SPACE)
-            break;
-        if (chess[y][x-i]==nowcolor)
-            Ltemp=1;
+        if (x-i<0||chess[y][x-i]==SPACE){
+            Ltemp=0;break;}
+        if (chess[y][x-i]==(nowcolor)){
+            break;}
+        if (chess[y][x-i]==nowcolor%2+1){
+            Ltemp++;}
     }
-    if(chess[y][x-1]==nowcolor||chess[y][x-1]==SPACE) //若當前落子位置左邊一顆為當前落子顏色 則以左邊來看無法落子
+    if(chess[y][x-1]==nowcolor||chess[y][x-1]==SPACE) //若當前落子位置左邊一顆為當前落子顏色或空白 則以左邊來看無法落子
         Ltemp=0;
-
+    
     i=0;
-    while(1){
+    while(1){//右
         i++;
-        if (x+i>7||chess[y][x+i]==SPACE)
-            break;
+        if (x+i>7||chess[y][x+i]==SPACE){
+            Rtemp=0;break;}
         if (chess[y][x+i]==nowcolor)
-            Rtemp=1;
+            break;
+        if (chess[y][x+i]==nowcolor%2+1)
+            Rtemp++;
     }
     if(chess[y][x+1]==nowcolor||chess[y][x+1]==SPACE) 
         Rtemp=0;
@@ -80,10 +94,12 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
     i=0;
     while(1){//上
         i++;
-        if (y-i<0||chess[y-i][x]==SPACE)
-            break;
+        if (y-i<0||chess[y-i][x]==SPACE){
+            Utemp=0;break;}
         if (chess[y-i][x]==nowcolor)
-            Utemp=1;
+            break;
+        if (chess[y-i][x]==nowcolor%2+1)
+            Utemp++;
     }
     if(chess[y-1][x]==nowcolor||chess[y-1][x]==SPACE) 
         Utemp=0; 
@@ -91,10 +107,12 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
     i=0;
     while(1){//下
         i++;
-        if (y+i>7||chess[y+i][x]==SPACE)
-            break;
+        if (y+i>7||chess[y+i][x]==SPACE){
+            Dtemp=0;break;}
         if (chess[y+i][x]==nowcolor)
-            Dtemp=1;
+            break;
+        if (chess[y+i][x]==nowcolor%2+1)
+            Dtemp++;
     }
     if(chess[y+1][x]==nowcolor||chess[y+1][x]==SPACE) 
         Dtemp=0;
@@ -102,11 +120,12 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
     i=0;
     while(1){//右下
         i++;
-        if (x+i>7||y+i>7||chess[y+i][x+i]==SPACE)
+        if (x+i>7||y+i>7||chess[y+i][x+i]==SPACE){
+            RDtemp=0;break;}
+        if (chess[y+i][x+i]==nowcolor)
             break;
-        if (chess[y+i][x+i]==nowcolor){
-            RDtemp=1;
-            break;
+        if (chess[y+i][x+i]==nowcolor%2+1){
+            RDtemp++;
         }
     }
     if(chess[y+1][x+1]==nowcolor||chess[y+1][x+1]==SPACE) 
@@ -115,11 +134,12 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
     i=0;
     while(1){//右上
         i++;
-        if (x+i>7||y-i<0||chess[y-i][x+i]==SPACE)
+        if (x+i>7||y-i<0||chess[y-i][x+i]==SPACE){
+            RUtemp=0;break;}
+        if (chess[y-i][x+i]==nowcolor)
             break;
-        if (chess[y-i][x+i]==nowcolor){
-            RUtemp=1;
-            break;
+        if (chess[y-i][x+i]==nowcolor%2+1){
+            RUtemp++;
         }
     }
     if(chess[y-1][x+1]==nowcolor||chess[y-1][x+1]==SPACE) 
@@ -128,11 +148,12 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
     i=0;
     while(1){//左下
         i++;
-        if (x-i<0||y+i>7||chess[y+i][x-i]==SPACE)
+        if (x-i<0||y+i>7||chess[y+i][x-i]==SPACE){
+            LDtemp=0;break;}
+        if (chess[y+i][x-i]==nowcolor)
             break;
-        if (chess[y+i][x-i]==nowcolor){
-            LDtemp=1;
-            break;
+        if (chess[y+i][x-i]==nowcolor%2+1){
+            LDtemp++;
         }
     }
     if(chess[y+1][x-1]==nowcolor||chess[y+1][x-1]==SPACE) 
@@ -141,18 +162,21 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
     i=0;
     while(1){//左上
         i++;
-        if (x-i<0||y-i<0||chess[y-i][x-i]==SPACE)
+        if (x-i<0||y-i<0||chess[y-i][x-i]==SPACE){
+            LUtemp=0;break;}
+        if (chess[y-i][x-i]==nowcolor)
             break;
-        if (chess[y-i][x-i]==nowcolor){
-            LUtemp=1;
-            break;
+        if (chess[y-i][x-i]==nowcolor%2+1){
+            LUtemp++;
         }
     }
     if(chess[y-1][x-1]==nowcolor||chess[y-1][x-1]==SPACE) 
         LUtemp=0;
     
-
-
+    if (total<Ltemp+Rtemp+Utemp+Dtemp+LUtemp+LDtemp+RUtemp+RDtemp){
+        total=Ltemp+Rtemp+Utemp+Dtemp+LUtemp+LDtemp+RUtemp+RDtemp;
+        maxX=x,maxY=y;
+    }
     if (Ltemp+Rtemp+Utemp+Dtemp+LUtemp+LDtemp+RUtemp+RDtemp==0){
         return 0;
     }else{
@@ -163,7 +187,9 @@ int yes_or_no(int y,int x,int nowcolor) {//檢測是否可以落子
 
 void reverse(int y,int x,int nowcolor){//翻轉棋子
     int i=0;
-    if (Ltemp==1){
+    chess[y][x]=nowcolor;
+    yes_or_no(y,x,nowcolor);
+    if (Ltemp!=0){
         while(1){
             i++;
             if (chess[y][x-i]==nowcolor)
@@ -172,7 +198,7 @@ void reverse(int y,int x,int nowcolor){//翻轉棋子
                 chess[y][x-i]=nowcolor;
         }
     }
-    if (Rtemp==1){
+    if (Rtemp!=0){
         while(1){
             i++;
             if (chess[y][x+i]==nowcolor)
@@ -181,7 +207,7 @@ void reverse(int y,int x,int nowcolor){//翻轉棋子
                 chess[y][x+i]=nowcolor;
         }
     }
-    if (Utemp==1){
+    if (Utemp!=0){
         while(1){
             i++;
             if (chess[y-i][x]==nowcolor)
@@ -190,7 +216,7 @@ void reverse(int y,int x,int nowcolor){//翻轉棋子
                 chess[y-i][x]=nowcolor;
         }
     }
-    if (Dtemp==1){
+    if (Dtemp!=0){
         while(1){
             i++;
             if (chess[y+i][x]==nowcolor)
@@ -199,7 +225,7 @@ void reverse(int y,int x,int nowcolor){//翻轉棋子
                 chess[y+i][x]=nowcolor;
         }
     }
-    if (RDtemp==1){
+    if (RDtemp!=0){
         while(1){
             i++;
             if (chess[y+i][x+i]==nowcolor)
@@ -208,7 +234,7 @@ void reverse(int y,int x,int nowcolor){//翻轉棋子
                 chess[y+i][x+i]=nowcolor;
         }
     }
-    if (RUtemp==1){
+    if (RUtemp!=0){
         while(1){
             i++;
             if (chess[y-i][x+i]==nowcolor)
@@ -217,7 +243,7 @@ void reverse(int y,int x,int nowcolor){//翻轉棋子
                 chess[y-i][x+i]=nowcolor;
         }
     }
-    if (LDtemp==1){
+    if (LDtemp!=0){
         while(1){
             i++;
             if (chess[y+i][x-i]==nowcolor)
@@ -226,7 +252,7 @@ void reverse(int y,int x,int nowcolor){//翻轉棋子
                 chess[y+i][x-i]=nowcolor;
         }
     }
-    if (LUtemp==1){
+    if (LUtemp!=0){
         while(1){
             i++;
             if (chess[y-i][x-i]==nowcolor)
@@ -258,43 +284,54 @@ int scanboard(){//檢查棋盤是否還有可落子的地方
 
 void wherecan(int nowcolor){//印出白子黑子可下位置 並讀入落子位置
     for(int i=1;i<3;i++){
-            if (i==WHITE){
-                printf("白子可下 : ");
-                nowcolor = WHITE;
-            }
-            else if (i==BLACK){
-                printf("\n黑子可下 : ");
-                nowcolor = BLACK;
-            }
-
-            for(int j=0;j<8;j++){
-                for(int k=0;k<8;k++){
-                    if(chess[k][j]!=0)
-                        continue;
-                    if(yes_or_no(k,j,nowcolor)!=0)
-                        printf("(%d,%d)",j,k);
-                }
-            }
+        if (i==WHITE){
+            printf("白子可下 : ");
+            nowcolor = WHITE;
         }
-        printf("\n請落子(x,y):");
-        scanf("%d%d",&newX,&newY);//掃描要落子的位置
-
-        if (newX>7||newX<0||newY>7||newY<0){
-            printf("不可落子");
-            return;
+        else if (i==BLACK){
+            printf("\n黑子可下 : ");
+            nowcolor = BLACK;
         }
 
-        if (chess[newY][newX]==SPACE){
-            printf("空白\n");
+        for(int j=0;j<8;j++){
+            for(int k=0;k<8;k++){
+                if(chess[k][j]!=0)
+                    continue;
+                if(yes_or_no(k,j,nowcolor)!=0)
+                    printf("(%d,%d)",j,k);
+            }
         }
-        else if (chess[newY][newX]==BLACK){
-            printf("黑\n");
-        }
-        else{ 
-            printf("白\n");
-        }
+    }
+    printf("\n換您落子了 請落子(x,y):");
+    scanf("%d%d",&newX,&newY);//掃描要落子的位置
+
+    if (newX>7||newX<0||newY>7||newY<0){
+        printf("\n不可落子");
+        return;
+    }
+
+    if (chess[newY][newX]==SPACE){
+        printf("空白\n");
+    }
+    else if (chess[newY][newX]==BLACK){
+        printf("黑\n");
+    }
+    else{ 
+        printf("白\n");
+    }
 }
 
-void computurn(){//電腦下 未完成
-
+void AIturn(int nowcolor){//電腦下 未完成
+    maxX=0,maxY=0,total=0;
+    for(int j=0;j<8;j++){
+        for(int k=0;k<8;k++){
+            if(chess[k][j]!=0)
+                continue;
+            if(yes_or_no(k,j,nowcolor)!=0)
+                printf("(%d,%d)",j,k);
+        }
+    }
+    reverse(maxY,maxX,nowcolor);
+    printf("電腦下(%d,%d),翻轉了%d個子\n",maxX,maxY,total);
+    
 }
